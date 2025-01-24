4d660df014500bc068ef4adbf523e4e65e0df0e2
Diego Marino <diego.marino@ocuco.com>
2025-01-24 17:33:06 -0300

    Add binding for telescope grep_string

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -38,10 +38,42 @@ return {
           }
         }
       })
+
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<C-f>", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+
+      -- Find word under cursor
+      local telescopebuiltin = require("telescope.builtin")
+      local opts = { noremap = true, silent = true }
+
+      -- This can be extracted for reuse purpose
+      function vim.getVisualSelection()
+        vim.cmd('noau normal! "vy"')
+        local text = vim.fn.getreg('v')
+        vim.fn.setreg('v', {})
+
+        text = string.gsub(text, "\n", "")
+        if #text > 0 then
+          return text
+        else
+          return ''
+        end
+      end
+
+      -- Grep word under cursor and expand
+      local function grep_cword()
+        return telescopebuiltin.grep_string({search = vim.fn.expand("<cword>")})
+      end
+
+      vim.keymap.set('v', '<leader>f', function()
+        local text = vim.getVisualSelection()
+        telescopebuiltin.grep_string({ search = text })
+      end, opts)
+      vim.keymap.set("n", "<leader>f", grep_cword, {})
+      vim.api.nvim_create_user_command("GrepCword", grep_cword, {})
+      -- ############################## --
 
       require("telescope").load_extension("ui-select")
     end,
