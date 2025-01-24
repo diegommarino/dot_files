0ed66c5e1758b028a6aba03bfa3d442e15a41a95
Diego Marino <diego.marino@ocuco.com>
2025-01-24 15:03:00 -0300

    Adds more lazy plugins

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'auto', -- You can choose a theme you like
+        section_separators = '',
+        component_separators = ''
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'}, -- This will show the current Git branch
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
+    }
+  end,
+}
+
