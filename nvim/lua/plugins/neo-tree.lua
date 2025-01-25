df2c22261b044e3089ed445ff24971f721d783b6
Diego Marino <diegommarino@gmail.com>
2025-01-25 14:03:51 -0300

    Add config to show all buffers in neo-tree and also adds vim-fugitive

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,9 +6,22 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	config = function()
-		vim.keymap.set("n", "<F2>", ":Neotree filesystem left toggle<CR>", {})
-		vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal float<CR>", {})
-		vim.keymap.set("n", "<F3>", ":Neotree reveal<CR>", {})
-	end,
+  config = function()
+    require('neo-tree').setup {
+      buffers = {
+        leave_dirs_open = true,
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+        bind_to_cwd = false,
+      },
+      filesystem = {
+        bind_to_cwd = false,
+      },
+    }
+    vim.keymap.set("n", "<F2>", ":Neotree filesystem left toggle<CR>", {})
+    vim.keymap.set("n", "<leader>b", ":Neotree buffers reveal float<CR>", {})
+    vim.keymap.set("n", "<F3>", ":Neotree reveal<CR>", {})
+  end,
 }
