f6b1f722ca80ad23bff3a2eb32eaaf4bc4b9f690
Diego Marino <diego.marino@ocuco.com>
2025-01-28 17:34:35 -0300

    Test new lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -2,6 +2,7 @@ return {
   {
     "williamboman/mason.nvim",
     lazy = false,
+    opts = { ensure_installed = { "erb-formatter", "erb-lint" } },
     config = function()
       require("mason").setup()
     end,
@@ -11,6 +12,7 @@ return {
     lazy = false,
     opts = {
       auto_install = true,
+      ensure_installed = { 'solargraph' }
     },
   },
   {
@@ -18,12 +20,26 @@ return {
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- local registry = require('mason-registry')
+      -- local solargraph = registry.get_package('solargraph')
+      -- local solargraph_path = solargraph:get_install_path()
+      -- local rubocop_config = solargraph_path .. "/lib/.rubocop.yml"
 
       local lspconfig = require("lspconfig")
       lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
       lspconfig.solargraph.setup({
+        -- cmd = { solargraph_path .. "/bin/solargraph", "stdio" },
+        -- settings = {
+        --   solargraph = {
+        --     diagnostics = true,
+        --     rubocop = {
+        --       command = solargraph_path .. "/bin/rubocop",
+        --       args = { "--config", rubocop_config }
+        --     }
+        --   }
+        -- },
         capabilities = capabilities
       })
       lspconfig.html.setup({
@@ -37,6 +53,29 @@ return {
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.g.lazyvim_ruby_lsp = "solargraph"
+      vim.g.lazyvim_ruby_formatter = "rubocop"
     end,
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        ruby_lsp = {
+          enabled = lsp == "ruby_lsp",
+        },
+        solargraph = {
+          enabled = lsp == "solargraph",
+        },
+        rubocop = {
+          -- If Solargraph and Rubocop are both enabled as an LSP,
+          -- diagnostics will be duplicated because Solargraph
+          -- already calls Rubocop if it is installed
+          enabled = formatter == "rubocop" and lsp ~= "solargraph",
+        },
+        standardrb = {
+          enabled = formatter == "standardrb",
+        },
+      },
+    },
   },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,38 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+    dependencies = { "RRethy/nvim-treesitter-endwise" },
+    opts = function(_, opts)
+      opts.endwise = { enable = true }
+      opts.highlight = { enable = true }
+      opts.auto_install = { enable = true }
+      opts.indent = { enable = true, disable = { "yaml", "ruby" } }
+      opts.ensure_installed = {
+        "bash",
+        "embedded_template",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      }
+    end,
+    -- config = function()
+    --   local config = require("nvim-treesitter.configs")
+    --   config.setup({
+    --     auto_install = true,
+    --     highlight = { enable = true },
+    --     indent = { enable = true },
+    --   })
+    -- end,
+  },
 }
