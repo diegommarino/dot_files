c05551d54cbf83c4a7becb50f1407a738ba8eff1
Diego Marino <diego.marino@ocuco.com>
2025-03-06 15:14:06 -0300

    Adds tmux plugins

--- nvim/lua/plugins/vim-gitgutter.lua
+++ nvim/lua/plugins/vim-gitgutter.lua
@@ -3,6 +3,10 @@ return {
     "airblade/vim-gitgutter",
     config = function()
       vim.cmd("let g:gitgutter_sign_allow_clobber = 1")
+      vim.keymap.set('n', '<leader>g>', '<Plug>(GitGutterNextHunk)', { noremap = true, silent = true })  -- Next hunk
+      vim.keymap.set('n', '<leader>g<', '<Plug>(GitGutterPrevHunk)', { noremap = true, silent = true })  -- Previous hunk
+      vim.keymap.set('n', '<leader>gs', '<Plug>(GitGutterStageHunk)', { noremap = true, silent = true }) -- Stage hunk
+      vim.keymap.set('n', '<leader>gu', '<Plug>(GitGutterUndoHunk)', { noremap = true, silent = true })  -- Undo hunk
     end,
   },
 }
