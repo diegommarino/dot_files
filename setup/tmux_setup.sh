cfc8c4c55e845bc7b07d638b93c866527edcab5f
Diego Marino <diegommarino@gmail.com>
2025-04-06 17:45:29 -0300

    Fixes tmuxp setup

--- setup/tmux_setup.sh
+++ setup/tmux_setup.sh
@@ -3,9 +3,9 @@
 source ./setup/helpers.sh
 # Install tmux
 check_and_install_program tmux "sudo apt-get install -y tmux"
-check_and_install_program pip3 "sudo apt-get install -y python3-pip"
+check_and_install_program pipx "sudo apt-get install -y pipx"
 zsh
-check_and_install_program tmuxp "pip3 install --user tmuxp"
+check_and_install_program tmuxp "pipx install tmuxp"
 add_shared_config_line "source-file ~/.sharedcfg/.tmux.conf" ~/.tmux.conf
 mkdir -p ~/.tmuxp
 zsh
